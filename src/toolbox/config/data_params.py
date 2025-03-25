6a43f8d980d8b0cbb5f424865747ac7a4a4d0559
adamwa <adamwa@noc.ac.uk>
2025-03-25 11:12:48 +0000

    basic Data Processing param classes

--- src/toolbox/config/data_params.py
+++ src/toolbox/config/data_params.py
@@ -5,3 +5,22 @@ This module contains the dataclass definition for the data import and processing
 
 from dataclasses import dataclass
 from toolbox.utils.dynamic_classes import DynamicConfig
+
+
+@dataclass
+class FactoryCalibration(DynamicConfig):
+    """Factory Calibration parameters."""
+
+    pass
+
+
+class SensorPecificCorrection(DynamicConfig):
+    """Sensor Specific Correction parameters."""
+
+    pass
+
+
+class AutomatedCrossSensorAdjustment(DynamicConfig):
+    """Automated Cross Sensor Adjustment parameters."""
+
+    pass
